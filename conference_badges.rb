8078f005d5e3b979567fadb5389f7882cda56198
korey ko <koreykendrick1@gmail.com>
2020-05-29 02:51:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,9 @@ end
 def printer(attendees)
    badges_array = batch_badge_creator(attendees)
    room_array = assign_rooms(attendees)
- 
+ badges_array.each do |attendees_name| 
+    puts attendees_name
+  end
   room_array.each do|room_assignment|
 end
   puts room_assignment

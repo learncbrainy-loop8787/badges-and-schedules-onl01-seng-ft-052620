4cc84ef0c23f5262e0ff6e06ce9b731bbdc1d700
korey ko <koreykendrick1@gmail.com>
2020-05-29 02:09:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+ badges =[]
+  attendees.each do |name_string|
+    badges.push("Hello, my name is #{name_string}.")
+    end
+   badges
+end
+
+def assign_rooms(attendees)
+  counter = 1
+  room_assignments = []
+  attendees.each do |name_string|
+    room_assignments.push("Hello, #{name_string}! You'll be assigned to room #{counter}!")
+    counter += 1
+     end
+     room_assignments
+end
+
+def printer(attendees)
+  print batch_badge_creator
+  print assign_rooms
+end
+
+
+
+
